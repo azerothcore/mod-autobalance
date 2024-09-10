5d2778e301ae7684051cdc446efbe81c7ff2a79b
FingerKnoten <sebastian@sruweb.de>
2024-09-10 23:38:55 +0200

    Fix for broken chat messages #187 (#189)
    * Fix(Core/Misc): Acore::StringFormat to fmt format
    
    Fixes the issue that variables are not properly replaced.
    Instead placeholders %s,%u and %i were shown in chat.
    
    Closes issue #187
    
    * Fix(Core/Misc): Acore::StringFormat to fmt format
    
    Fixes the issue that variables are not properly replaced.
    Instead placeholders %.2f and %.3f were shown in chat.
    
    Closes issue #187
    
    * Adjusted format for float placehodlers
    
    Makes sure the original formatting for float
    placeholders still fits
    
    Closes issue #187

--- src/AutoBalance.cpp
+++ src/AutoBalance.cpp
@@ -6494,7 +6494,7 @@ public:
         {
             handler->PSendSysMessage("---");
             // Map basics
-            handler->PSendSysMessage("%s (%u-player %s) | ID %u-%u%s",
+            handler->PSendSysMessage("{} ({}-player {}) | ID {}-{}{}",
                                     player->GetMap()->GetMapName(),
                                     player->GetMap()->ToInstanceMap()->GetMaxPlayers(),
                                     player->GetMap()->ToInstanceMap()->IsHeroic() ? "Heroic" : "Normal",
@@ -6506,7 +6506,7 @@ public:
             // if (!mapABInfo->enabled) { return true; }
 
             // Player stats
-            handler->PSendSysMessage("Players on map: %u (Lvl %u - %u)",
+            handler->PSendSysMessage("Players on map: {} (Lvl {} - {})",
                                     mapABInfo->playerCount,
                                     mapABInfo->lowestPlayerLevel,
                                     mapABInfo->highestPlayerLevel
@@ -6603,7 +6603,7 @@ public:
         AutoBalanceCreatureInfo *targetABInfo=target->CustomData.GetDefault<AutoBalanceCreatureInfo>("AutoBalanceCreatureInfo");
 
         handler->PSendSysMessage("---");
-        handler->PSendSysMessage("%s (%u%s%s), %s",
+        handler->PSendSysMessage("{} ({}{}{}), {}",
                                   target->GetName(),
                                   targetABInfo->UnmodifiedLevel,
                                   isCreatureRelevant(target) && targetABInfo->UnmodifiedLevel != target->GetLevel() ? "->" + std::to_string(targetABInfo->selectedLevel) : "",
